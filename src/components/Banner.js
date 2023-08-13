8b0b66a14bb31d8db6ab7f893dec8b5955c4fd0b
AustinJ64 <austinj6497@gmail.com>
2023-08-12 21:32:12 -0500

    Assignment 6 completed

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return(
+    <div>
+        <header>"Orbit Report"</header>
+        <p>Click on the buttons to see the satellites in the selected orbit type</p>
+    </div>
+    );
+};
+
+export default Banner;
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,23 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((d) => {
+            return (
+              <tr key={d.id}>
+                <td>{d.name}</td>
+                <td>{d.type}</td>
+                <td>{d.launchDate}</td>
+                <td>{d.operational ? 'Active' : 'Inactive'}</td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
   );
